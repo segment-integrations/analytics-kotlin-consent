6690706354fa9b006a9a6ba0c48a6b51c566df04
Didier Garcia <digarcia@twilio.com>
2024-05-21 09:45:58 -0400

    Update to latest analytics-kotlin 1.16.1 and make the testapp more useful by adding the consent plugin to the timeline. (#10)

--- testapp/src/main/java/com/segment/analytics/destinations/mydestination/testapp/MainActivity.kt
+++ testapp/src/main/java/com/segment/analytics/destinations/mydestination/testapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.segment.analytics.destinations.mydestination.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "main-activity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.sendTrackEventButton)
+        button.setOnClickListener {v ->
+            val sdf = SimpleDateFormat("M/dd/yyyy hh:mm:ss.SSS")
+            val currentDate = sdf.format(Date())
+            MainApplication.analytics.track("Test Event ${currentDate}")
+
+            if (v != null) {
+                Snackbar.make(v, "Track Event created.", Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ testapp/src/main/java/com/segment/analytics/destinations/mydestination/testapp/MainApplication.kt
@@ -0,0 +1,60 @@
+package com.segment.analytics.destinations.mydestination.testapp
+
+import android.app.Application
+import com.segment.analytics.kotlin.core.Analytics
+import com.segment.analytics.kotlin.android.Analytics
+import com.segment.analytics.kotlin.core.platform.policies.CountBasedFlushPolicy
+import com.segment.analytics.kotlin.core.platform.policies.FrequencyFlushPolicy
+import com.segment.analytics.kotlin.destinations.consent.ConsentManager
+import org.json.JSONException
+import org.json.JSONObject
+import sovran.kotlin.SynchronousStore
+
+
+class MainApplication: Application() {
+
+    val SEGMENT_WRITE_KEY = "<writekey>"
+    companion object {
+        lateinit var analytics: Analytics
+        const val TAG = "application"
+    }
+
+    private fun getGroupIds(domainGroupData: JSONObject): List<String> {
+        val result: MutableList<String> = ArrayList()
+        try {
+            val groups = domainGroupData.getJSONArray("Groups")
+            for (i in 0 until groups.length()) {
+                val group = groups.getJSONObject(i)
+                val groupId = group.getString("OptanonGroupId")
+                result.add(groupId)
+            }
+        } catch (ex: JSONException) {
+            ex.printStackTrace()
+        }
+        return result
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Analytics.debugLogsEnabled = true
+        analytics = Analytics(SEGMENT_WRITE_KEY, applicationContext) {
+            this.collectDeviceId = true
+            this.trackApplicationLifecycleEvents = true
+            this.trackDeepLinks = true
+            this.flushPolicies = mutableListOf(
+                CountBasedFlushPolicy(1), // Flush after each event
+                FrequencyFlushPolicy(5000) // Flush after 5 Seconds
+            )
+        }
+
+        val consentCategoryProvider = CustomConsentCategoryProvider()
+        val store = SynchronousStore()
+
+        val consentPlugin = ConsentManager(store, consentCategoryProvider)
+
+        analytics.add(consentPlugin)
+    }
+
+
+}
